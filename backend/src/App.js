cd89cd44767b94d2238ce1571c5c504a2c62abee
Alperen Yilan <alp_eren1616@hotmail.com>
2023-06-11 20:32:24 +0300

    first commit

---
+++ backend/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Horizontalchart from './chart-horizontal'
+
+function App() {
+  return (
+    <div className="App">
+      <Horizontalchart/>
+    </div>
+  );
+}
+
+export default App;
